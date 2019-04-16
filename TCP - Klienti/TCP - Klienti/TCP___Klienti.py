dc0e396ae2785286e50edc9989cc4301d6146cfc
FatbardhGashi <48059724+FatbardhGashi@users.noreply.github.com>
2019-04-16 07:39:11 +0200

    Update TCP___Klienti.py

--- TCP - Klienti/TCP - Klienti/TCP___Klienti.py
+++ TCP - Klienti/TCP - Klienti/TCP___Klienti.py
@@ -1,41 +1,40 @@
 import socket
+import sys
+import io
 
-serverName = 'localhost'
-port = 12000
-soketi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soketi.connect((serverName, port))
+host='localhost'
+port=11000
 
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-print("Projekti i parë nga lëmda Rrjeta Kompjuterike")
-print("\tFIEK TCP Protokolli - Klienti")
+socketClient = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socketClient.connect((host,port))
+print("******************************************************************\n")
+print("Projekti i parë nga lënda Rrjeta Kompjuterike")
+print("\tFIEK TCP Protokolli - KLIENTI")
 print("\t\tFatbardh Gashi\n")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+print("******************************************************************\n")
 print("Metodat (funksionet) në dispozicion : \n")
 print("IPADRESA")
 print("NUMRIIPORTIT")
-print("BASHTINGELLORE{Hapsirë}Fjala/Fjalia")
-print("PRINTIMI{Hapsirë}Fjala/Fjalia")
+print("BASHTINGELLORE{Hapsirë}Teksti")
+print("PRINTIMI{Hapsirë}Teksti")
 print("EMRIIKOMPJUTERIT")
 print("KOHA")
 print("LOJA")
-print("KONVERTIMI{Hapsirë}Opcioni{Hapsirë}Vlera")
+print("KONVERTIMI{Hapsirë}Opconi{Hapsirë}Numri")
 print("\t*KilowattToHorsepower")
 print("\t*HorsepowerToKilowatt")
 print("\t*DegreesToRadians")
 print("\t*RadiansToDegrees")
 print("\t*GallonsToLiters")
 print("\t*LitersToGallons\n")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+print("******************************************************************\n")
 
-hyrja = input("Zgjedhni një kërkesë(metodë)")
+hyrja=input("Zgjedhni nje komande>>> ")
+while((hyrja != "") and (hyrja !="SHKYQU")):
+        socketClient.send(hyrja.encode())                
+        informata = socketClient.recv(128).decode()
 
-i=1
-while(i==1):
-    soketi.send(hyrja.encode())
-    informata = soketi.recv(128).decode()
-    print(informata)
-    hyrja = input("Zgjedhni një kërkesë(metodë)")
-    if(informata == "SHKYQU" and informata==""):
-        i=2
-print("Lidhja e klientit me serverin është shkyqur!")
-socket.close()
+        print(informata)
+        hyrja = input("Zgjedhni nje komande >>> ")
+
+socketClient.close();
